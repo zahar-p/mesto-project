40bdd2b6f4db04af53253626abbace309c52b59d
Zahar <Z@Z.Z>
2022-11-01 00:32:07 +0300

    webpack setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.css'
+
 const initialCards = [
   { name: 'Архыз', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg' },
   { name: 'Челябинская область', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg' },
